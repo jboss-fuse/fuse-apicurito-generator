59302d3b5a36ef0aab71a7e4bfcd43449d987861
Tom Cunningham <tcunning@redhat.com>
2019-05-13 13:47:22 -0400

    API changes for SB2

--- src/main/java/com/redhat/fuse/apicurio/Application.java
+++ src/main/java/com/redhat/fuse/apicurio/Application.java
@@ -17,7 +17,7 @@ package com.redhat.fuse.apicurio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication()
 public class Application extends SpringBootServletInitializer {
